d21090871226ca485d64fd4447476587070c17d8
wei.wang <wei.wang@LP00115.local>
2025-07-01 10:11:04 +0800

    add

---
+++ orm/db_init.py
@@ -0,0 +1,31 @@
+import sqlalchemy
+
+from sqlalchemy import create_engine
+
+def create_db_engine(db_url: str) -> sqlalchemy.engine.Engine:
+    engine = create_engine(db_url, echo=True)
+    return engine
+
+def init_db(engine: sqlalchemy.engine.Engine):
+    # from orm import models  # Import models to ensure they are registered with the metadata
+
+    metadata = sqlalchemy.MetaData()
+    metadata.create_all(engine)
+
+def create_tables(metadata: sqlalchemy.MetaData) -> sqlalchemy.Table:
+
+    # person = sqlalchemy.Table(
+    #     'person', metadata,
+    #     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),  
+    #     sqlalchemy.Column('name', sqlalchemy.String(50), nullable=False),
+    #     sqlalchemy.Column('age', sqlalchemy.Integer, nullable=False)
+    # )   
+
+    dwd_filtered_input = sqlalchemy.Table(
+        'dwd_filtered_input', metadata,
+        sqlalchemy.Column('ids', sqlalchemy.String(100)),  
+        sqlalchemy.Column('content', sqlalchemy.String(10000), nullable=False),
+    )   
+
+
+    return dwd_filtered_input
---
+++ orm/ops.py
@@ -0,0 +1,33 @@
+from db_init import create_db_engine, init_db, create_tables
+import sqlalchemy
+
+if __name__ == "__main__":
+    # Example usage
+    # db_url = "sqlite:///example.db"  # Replace with your actual database URL
+    db_url = "mysql+pymysql://artgent_user:ArtgentData789@rm-uf6mb1y1i2970r1goao.mysql.rds.aliyuncs.com/artgent"  # Example MySQL URL
+    engine = create_db_engine(db_url)
+    
+    # Initialize the database and create tables
+    init_db(engine)
+    
+    # Create tables using the metadata
+    metadata = sqlalchemy.MetaData()
+    dwd_filtered_input = create_tables(metadata)
+    
+    print("Tables created:", dwd_filtered_input.name)
+
+    with engine.connect() as connection:
+        # metadata.create_all(connection)
+        
+        # insert_query = dwd_filtered_input.insert().values(ids="1", content='{"a":"aValue"}')
+        # connection.execute(insert_query)
+        # connection.commit() 
+
+        query = dwd_filtered_input.select()
+        result = connection.execute(query)
+
+        for row in result:
+            # print(row)
+            print(row.ids, row.content)
+
+
